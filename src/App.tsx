795e111cb9b61d7a00a26e5c0f70a49a87cd6028
mamun8280 <mdmamun769158@gmail.com>
2026-09-12 12:11:51 +0600

    jason data

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@
 import Nav from "./Component/Nav";
 import Banner from "./Component/Banner";
 import Languages from "./Component/language/Languages";
-import Footer from "./Component/footer";
+import Footer from "./Component/Footer";
+import { Suspense } from "react";
+import type { Ilanguage } from "./types/Types";
 
+const languagesFetch = async(): Promise<Ilanguage[]> => {
+  const res = await fetch("/data.json");
+  const data = await res.json();
+  return data;
+}
 
 function App() {
 
+  const languagesPromise = languagesFetch()
 
   return (
    
@@ -14,7 +22,9 @@ function App() {
     <div>
       <Nav />
       <Banner />
-      <Languages />
+      <Suspense fallback={<h2>Loading languages...</h2>}>
+        <Languages languagesPromise={languagesPromise} />
+      </Suspense>
       <Footer />
 
     </div>
--- src/Component/language/Languages.tsx
+++ src/Component/language/Languages.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { use } from 'react';
+import type { Ilanguage } from '../../types/Types';
 
-const Languages = () => {
+interface LanguagesProps{
+    languagesPromise : Promise<Ilanguage[]>;
+}
+
+const Languages = ({languagesPromise}:LanguagesProps) => {
+    console.log({languagesPromise});
+    const languages =use(languagesPromise);
     return (
        <div className="bg-red-500 p-10 text-2xl text-center text-white">
       languages card
     </div>
     );
 };
+
 
 export default Languages;
